2b82da8bf17cc02d88a862d7f2302ed5ebfac2e5
Valdrin Jonuzi <valdrinj00@gmail.com>
2024-03-09 18:08:58 -0800

    Prepare to access station conditions via the API

--- 📈_Dashboard.py
+++ 📈_Dashboard.py
@@ -1,6 +1,32 @@
 import streamlit as st
 import pandas as pd
 from components import bar, gauge, rose
+from urllib.parse import urlencode, urlunparse
+import json, requests
+
+api_params = {
+    'device_id': st.secrets['device_id'],
+    'password': st.secrets['password'],
+    'api_key': st.secrets['api_key']
+}
+
+# Build URL according to the APIv1 documentation
+# https://www.weatherlink.com/static/docs/APIdocumentation.pdf
+request_url = urlunparse(
+    (
+        'https',
+        'api.weatherlink.com',
+        '/v1/NoaaExt.json',
+        '',
+        urlencode(api_params),
+        ''
+    )
+)
+
+try:
+    current_conditions = json.loads(requests.get(request_url).text)
+except:
+    pass
 
 st.title("Weather Station")
 
